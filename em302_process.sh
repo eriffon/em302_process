cd79a7e914e912079fb8775884382d87035548c2
Jean-Guy Nistad <jean-guy.nistad@hcu-hamburg.de>
2014-07-17 16:53:40 -0400

    added functions shiptrack() and webtide2caris() and modified the command line argument parsing function to use -- for better usage clarity.

--- em302_process.sh
+++ em302_process.sh
@@ -11,8 +11,6 @@
 # PROCESS_ALL() - Process all .all files to generate mb59 files
 #
 process_all() {
-    touch $LOG
-
     printf "Processing all .all files in directory %s...\n" $DIR_DATA_ALL | tee $LOG
 
     # Create the .all datalist
@@ -89,157 +87,25 @@ process_all() {
     printf "done.\n" | tee -a $LOG
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #
-# Merge all mb59 files with gsf files
+# SHIPTRACK(MB-system datalist) - Create a shiptrack from an MB-System datalist
 #
-merge_gsf() {
-    printf "Merging all .gsf files...\n"
-
-    # Create the .gsf datalist
-    ls -1 $DIR_DATA_GSF | grep 'Amundsen.gsf$' | awk '{print $1 " 121 1.000000"}' > $DIR_DATA_GSF/$DATALIST_GSF
-    printf "Created datalist_gsf.mb-1 successfully\n"
-
-    # Create the merge gsf script
-    touch $MERGE_GSF_SCRIPT | printf "#!/bin/bash\n\n" > $MERGE_GSF_SCRIPT
-    ls -1 $DIR_DATA_GSF | grep 'Amundsen.gsf$' | awk '{sub(".gsf",""); print $1}' | \
-	                                         awk -v dir_mb59=$DIR_DATA_MB59 -v dir_gsf=$DIR_DATA_GSF -v dir_mb59e=$DIR_DATA_MB59E \
-                                                 '{print "mbcopy -F59/59/121 -I " dir_mb59 "/" $1 ".mb59 -M " dir_gsf "/" $1 ".gsf -O " dir_mb59e "/" $1 ".mb59"}' >> $MERGE_GSF_SCRIPT
-
-    # run the merge gsf script
-    chmod +x $MERGE_GSF_SCRIPT
-    source $MERGE_GSF_SCRIPT
-    printf "Done merging .gsf files\n"
-
-    # Create the mb59e datalist
-    ls -1 $DIR_DATA_MB59E | grep 'Amundsen.mb59$' | awk '{print $1 " 59 1.000000"}' > $DIR_DATA_MB59E/$DATALIST_MB59E
-    printf "Created datalist_mb59.mb-1 successfully\n"
-
-    # Create ancillary files
-    mbdatalist -F-1 -I $DIR_DATA_MB59E/$DATALIST_MB59E -N
-    printf "Done creating ancillary files for mb59e files\n"
+shiptrack() {
+    printf "Creating a shiptrack for the %s MB-System datalist..." $1 | tee -a $LOG
+    TIMESTAMP=$(date --utc +%Y%m%d-%H%M%S).txt
+    mbnavlist -F-1 -I $1 -D600 -OXYJ | awk '{$1=$1}1' > $DIR_SHIPTRACK/$SHIPTRACK_PREFIX-$TIMESTAMP
+    printf "done.\n" | tee -a $LOG
+    printf "The shiptrack file %s was created in the %s directory\n" $SHIPTRACK_PREFIX-$TIMESTAMP $DIR_SHIPTRACK | tee -a $LOG
 }
 
 #
-# Process unprocessed .all files
-#
-update_process_all() {
-    printf "Processing new files...\n"
-   
-    # Creating new unprocessed .all files datalist
-    find $DIR_DATA_ALL -type f -mmin +30 -printf "%f\n" | sort | grep 'Amundsen.all$' > fulllist
-    cat $DIR_DATA_ALL/$DATALIST_ALL | cut -d' ' -f1 > processedlist
-    comm -13 processedlist fulllist > unprocessedlist
-    if [ ! -s "unprocessedlist" ]
-    then
-	printf "No new files to process. Exiting...\n"
-	cleanup
-	exit 0
-    fi
-    cat unprocessedlist | awk '{print $1 " 58 1.000000"}' > $DIR_DATA_ALL/$UNPROCESSED_DATALIST_ALL
-    printf "Done creating new unprocessed .all files datalist...\n"
-
-    # Running mbkongsbergpreprocess on unprocessed .all files
-    mbkongsbergpreprocess -C -D $DIR_DATA_MB59 -F-1 -I $DIR_DATA_ALL/$UNPROCESSED_DATALIST_ALL
-    cat $DIR_DATA_ALL/$UNPROCESSED_DATALIST_ALL >> $DIR_DATA_ALL/$DATALIST_ALL
-    rm $DIR_DATA_ALL/$UNPROCESSED_DATALIST_ALL
-    printf "Done running mbkongsbergpreprocess...\n"
-
-    # Creating a new unprocessed .mb59 files datalist
-    cat unprocessedlist | awk '{sub(".all",".mb59"); print}' | awk '{print $1 " 59 1.000000"}' > $DIR_DATA_MB59/$UNPROCESSED_DATALIST_MB59
-    printf "Done creating new unprocessed .mb59 files datalist...\n"
-    
-    # Creating a shiptrack with newly processed files
-    mbnavlist -F-1 -I $DIR_DATA_MB59/$UNPROCESSED_DATALIST_MB59 -D600 -OXYJ | awk '{$1=$1}1' > $DIR_SHIPTRACK/$SHIPTRACK_PREFIX_$(date --utc +%Y%m%d-%H%M%S).txt
-    printf "Done creating a shiptrack with newly processed files...\n"
-
-    # Append unprocessed mb59 list to processed mb59 list
-    cat $DIR_DATA_MB59/$UNPROCESSED_DATALIST_MB59 >> $DIR_DATA_MB59/$DATALIST_MB59
-
-    # Clean up temporary files when done
-    rm fulllist processedlist unprocessedlist
-}
-
-#
-# Merge unmerged mb59 files with gsf files
-#
-update_merge_gsf() {
-    # Creating new unmerged .gsf files datalist
-    ls -1 $DIR_DATA_GSF | grep 'Amundsen.gsf$' > fulllist
-    cat $DIR_DATA_GSF/$DATALIST_GSF | cut -d' ' -f1 > mergedlist
-    comm -13 mergedlist fulllist > unmergedlist
-    if [ ! -s "unmergedlist" ]
-    then
-	printf "No new files to merge. Exiting...\n"
-	rm fulllist mergedlist unmergedlist
-	exit 0
-    fi
-    cat unmergedlist | awk '{print $1 " 121 1.000000"}' > $DIR_DATA_GSF/$UNPROCESSED_DATALIST_GSF
-    printf "Done creating new unmerged .gsf files datalist...\n"
-
-    # Create the merge gsf script
-    touch $MERGE_GSF_SCRIPT | printf "#!/bin/bash\n\n" > $MERGE_GSF_SCRIPT
-    ls -1 $DIR_DATA_GSF | cat unmergedlist | awk '{sub(".gsf",""); print $1}' | \
-	                                         awk -v dir_mb59=$DIR_DATA_MB59 -v dir_gsf=$DIR_DATA_GSF -v dir_mb59e=$DIR_DATA_MB59E \
-                                                 '{print "mbcopy -F59/59/121 -I " dir_mb59 "/" $1 ".mb59 -M " dir_gsf "/" $1 ".gsf -O " dir_mb59e "/" $1 ".mb59"}' >> $MERGE_GSF_SCRIPT
-
-    # run the merge gsf script
-    chmod +x $MERGE_GSF_SCRIPT
-    source $MERGE_GSF_SCRIPT
-    printf "Done merging .gsf files\n"
-
-    # Append unprocessed gsf list to processed gsf list
-    cat $DIR_DATA_GSF/$UNPROCESSED_DATALIST_GSF >> $DIR_DATA_GSF/$DATALIST_GSF
-
-    # Create the newly created mb59e datalist
-    cat unmergedlist | awk '{sub(".gsf",".mb59"); print}' | awk '{print $1 " 59 1.000000"}' > $DIR_DATA_MB59E/$DATALIST_MB59E
-    printf "Created datalist_mb59.mb-1 successfully\n"
-
-    # Create ancillary files
-    mbdatalist -F-1 -I $DIR_DATA_MB59E/$DATALIST_MB59E -N
-    printf "Done creating ancillary files for newly created mb59e files\n"
-
-    # Clean up temporary files when done
-    rm fulllist unmergedlist mergedlist
-}
-
-#
-# Create a CARIS HIPS & SIPS tide file from the webtide output
+# WEBTIDE2CARIS() - Convert the Webtide prediction file to a CARIS HIPS & SIPS tide file
 #
 webtide2caris() {
+    printf "Converting the Webtide prediction file to a CARIS HIPS & SIPS tide file..." | tee -a $LOG
     # Check that the track prediction file exists
     if [ ! -f $DIR_WEBTIDE/Track\ Elevation\ Prediction\ \(Time\ in\ GMT\).html ]; then
-	printf "Warning! File Track Elevation Prediction (Time in GMT).html not found! Have you copied the output from Webtide to the %s directory?\n" $DIR_WEBTIDE
+	printf "Warning! File Track Elevation Prediction (Time in GMT).html not found! Have you copied the output from Webtide to the %s directory?\n" $DIR_WEBTIDE  | tee -a $LOG
 	exit 1
     fi
 
@@ -248,17 +114,23 @@ webtide2caris() {
 
     # Generate the CARIS HIPS & SIPS tide file
     touch tempfile | echo "--------" > tempfile
+    TIMESTAMP=$(date --utc +%Y%m%d-%H%M%S).txt
     tail -n+8 $DIR_WEBTIDE/$WEBTIDE_NAME | head -n-3 | sed '/<\/p><pre>/,/<\/pre>/ s/<\/p><pre>//g' | \
 	awk '{$1=$1}1' | cut -d' ' -f1,4-8 | awk 'BEGIN {FS = OFS = " " } {print $2, $3, $4, $5, $6, $1}' | \
 	awk '{cmd ="date \"+%Y/%m/%d\" -d \"$(date +%Y)-01-01 $(("$2" - 1))days\""; cmd | getline var; print var " " $3 ":" $4 ":" $5 " " $6; close(cmd)}' >> tempfile 
-    awk 'BEGIN { FS = "\n"; OFS = "\r\n" } { $1 = $1; print }' tempfile > $DIR_HIPS_TIDE/$HIPS_TIDE_PREFIX_$(date --utc +%Y%m%d-%H%M%S).tid
+    awk 'BEGIN { FS = "\n"; OFS = "\r\n" } { $1 = $1; print }' tempfile > $DIR_HIPS_TIDE/$HIPS_TIDE_PREFIX-$TIMESTAMP
     rm tempfile
-    printf "Done creating the CARIS HIPS & SIPS file...\n"
+    rm $DIR_WEBTIDE/$WEBTIDE_NAME
+    printf "done.\n" | tee -a $LOG
+    printf "The CARIS HIPS & SIPS file %s was created in the %s directory\n" $HIPS_TIDE_PREFIX_$TIMESTAMP $DIR_WEBTIDE | tee -a $LOG
 }
 
-# 
+
+####################
+# MAIN STARTS HERE #
+####################
+
 # Set the project Metadata
-#
 if [ ! -f parameters.dat ]; then
     printf "Warning! No parameter file found: parameters.dat\n"
     exit 1
@@ -266,39 +138,45 @@ fi
 chmod +x parameters.dat
 source parameters.dat
 
-#
+# Check if log file exists. Create it if first run
+if [ ! -f $LOG ]; then
+    touch $LOG
+fi
+
 # Parse the command line
-#
-while getopts :agutw option
-do
-    case "${option}" in
-	a)   verbose=true
-	     quiet=
-	     process_all
-	     ;;
-	g)   verbose=true
-	     quiet=
-	     merge_gsf
-	     ;;
-        u)   verbose=true
-	     quiet=
-	     update_process_all
-	     ;;
-        t)   verbose=true
-	     quiet=
-	     webtide2caris
-	     ;;
-        w)   verbose=true
-	     quiet=
-	     update_merge_gsf
-	     ;;
-	'?') printf "Usage: $0 [-agtuw]\n"
-	     printf "Use the -a option to process all .all files.\n"
-	     printf "Use the -g option to merge all gsf files with mb59 files.\n"
-	     printf "Use the -t option to create a CARIS HIPS & SIPS tide file with the Webtide file Track Elevation Prediction (Time in GMT).html located in %s\n" $DIR_WEBTIDE/
-	     printf "Use the -u option to process all unprocessed .all files.\n"
-	     printf "Use the -w option to merge all unmerged .gsf files.\n"
-	     exit 1
+OPTS=`getopt -o h -l all -l track: -l tide -l help -- "$@"`
+if [ $? != 0 ]
+then
+    exit 1
+fi
+
+eval set -- "$OPTS"
+
+while true ; do
+    case "$1" in
+        -h) printf "Usage: $0 [--all] [--track FILE] [--tide] [--help]\n";
+	    shift;;
+
+        --all)
+	    process_all;
+	    shift;;
+	
+	--track)
+	    shiptrack $2;
+	    shift 2;;
+
+	--tide)
+	    webtide2caris;
+	    shift;;
+
+	--help) 
+	    printf "Usage: $0 [--all] [--track FILE] [--tide] [--help]\n\n"
+	    printf "** DESCRIPTION:\n"
+	    printf "Use the --all option to process all .all files.\n"
+	    printf "Use the --track option to create a shiptrack from the specified MB-system datalist\n"
+	    printf "Use the --tide option to create a CARIS HIPS & SIPS tide file with the Webtide file Track Elevation Prediction (Time in GMT).html located in %s\n" $DIR_WEBTIDE
+	    shift;;
+
+        --) shift; break;;
     esac
 done
-
